3996f6c9dbaedc67c00b97432f003ab9d85f1935
Johmiere <kimvalenzuela0421.jkv@gmail.com>
2024-12-23 09:49:11 +0800

    Add files via upload

---
+++ Valenzuela #28.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+your_name = "Valenzuela, Johmiere Kim"
+root = tk.Tk()
+root.title(f"OOP LA28 {your_name}")
+root.geometry("1000x1000000")
+root.configure(bg="pink")
+
+root.mainloop()
